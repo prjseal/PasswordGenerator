8a2d7201ee901e7a77498a82577de5b5f2a8b922
prjseal <prjseal@gmail.com>
2016-11-11 17:25:02 +0000

    first commit
    just adding the files to the project

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+/// <summary>
+/// Generates random passwords and validates that they meet the rules passed in
+/// </summary>
+public static class PasswordGenerator
+{
+    /// <summary>
+    /// Generates a random password based on the rules passed in the settings parameter
+    /// </summary>
+    /// <param name="settings">Password generator settings object</param>
+    /// <returns>Password or try again</returns>
+    public static string GeneratePassword(PasswordGeneratorSettings settings)
+    {
+        const int MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS = 2;
+        char[] password = new char[settings.PasswordLength];
+        int characterSetLength = settings.CharacterSet.Length;
+
+        System.Random random = new System.Random();
+        for (int characterPosition = 0; characterPosition < settings.PasswordLength; characterPosition++)
+        {
+            password[characterPosition] = settings.CharacterSet[random.Next(characterSetLength - 1)];
+
+            bool moreThanTwoIdenticalInARow =
+                characterPosition > MAXIMUM_IDENTICAL_CONSECUTIVE_CHARS
+                && password[characterPosition] == password[characterPosition - 1]
+                && password[characterPosition - 1] == password[characterPosition - 2];
+
+            if (moreThanTwoIdenticalInARow)
+            {
+                characterPosition--;
+            }
+        }
+
+        return string.Join(null, password);
+    }
+
+    /// <summary>
+    /// When you give it a password and some settings, it validates the password against the settings.
+    /// </summary>
+    /// <param name="settings">Password settings</param>
+    /// <param name="password">Password to test</param>
+    /// <returns>True or False to say if the password is valid or not</returns>
+    public static bool PasswordIsValid(PasswordGeneratorSettings settings, string password)
+    {
+        const string REGEX_LOWERCASE = @"[a-z]";
+        const string REGEX_UPPERCASE = @"[A-Z]";
+        const string REGEX_NUMERIC = @"[\d]";
+        const string REGEX_SPECIAL = @"([!#$%&*@\\])+";
+
+        bool lowerCaseIsValid = !settings.IncludeLowercase || (settings.IncludeLowercase && Regex.IsMatch(password, REGEX_LOWERCASE));
+        bool upperCaseIsValid = !settings.IncludeUppercase || (settings.IncludeUppercase && Regex.IsMatch(password, REGEX_UPPERCASE));
+        bool numericIsValid = !settings.IncludeNumbers || (settings.IncludeNumbers && Regex.IsMatch(password, REGEX_NUMERIC));
+        bool symbolsAreValid = !settings.IncludeSpecial || (settings.IncludeSpecial && Regex.IsMatch(password, REGEX_SPECIAL));
+
+        return lowerCaseIsValid && upperCaseIsValid && numericIsValid && symbolsAreValid;
+    }
+}
---
+++ PasswordGenerator/PasswordGeneratorSettings.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+/// <summary>
+/// Holds all of the settings for the password generator
+/// </summary>
+public class PasswordGeneratorSettings
+{
+    const string LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+    const string UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const string NUMERIC_CHARACTERS = "0123456789";
+    const string SPECIAL_CHARACTERS = @"!#$%&*@\";
+    const int PASSWORD_LENGTH_MIN = 8;
+    const int PASSWORD_LENGTH_MAX = 128;
+
+    public bool IncludeLowercase { get; set; }
+    public bool IncludeUppercase { get; set; }
+    public bool IncludeNumbers { get; set; }
+    public bool IncludeSpecial { get; set; }
+    public int PasswordLength { get; set; }
+    public string CharacterSet { get; set; }
+    public int MaximumAttempts { get; set; }
+
+    public PasswordGeneratorSettings(bool includeLowercase, bool includeUppercase, bool includeNumbers, bool includeSpecial, int passwordLength)
+    {
+        IncludeLowercase = includeLowercase;
+        IncludeUppercase = includeUppercase;
+        IncludeNumbers = includeNumbers;
+        IncludeSpecial = includeSpecial;
+        PasswordLength = passwordLength;
+
+        StringBuilder characterSet = new StringBuilder();
+
+        if (includeLowercase)
+        {
+            characterSet.Append(LOWERCASE_CHARACTERS);
+        }
+
+        if (includeUppercase)
+        {
+            characterSet.Append(UPPERCASE_CHARACTERS);
+        }
+
+        if (includeNumbers)
+        {
+            characterSet.Append(NUMERIC_CHARACTERS);
+        }
+
+        if (includeSpecial)
+        {
+            characterSet.Append(SPECIAL_CHARACTERS);
+        }
+
+        CharacterSet = characterSet.ToString();
+    }
+
+    /// <summary>
+    /// Checks that the password is within the valid length range
+    /// </summary>
+    /// <returns>A bool to say if it is valid or not</returns>
+    public bool IsValidLength()
+    {
+        return PasswordLength >= PASSWORD_LENGTH_MIN && PasswordLength <= PASSWORD_LENGTH_MAX;
+    }
+
+    /// <summary>
+    /// An error message string with the min and max length
+    /// </summary>
+    /// <returns>An error message string with the min and max length</returns>
+    public string LengthErrorMessage()
+    {
+        return $"Password length must be between {PASSWORD_LENGTH_MIN} and {PASSWORD_LENGTH_MAX} characters";
+    }
+}
