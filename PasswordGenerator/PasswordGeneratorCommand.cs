e95d1effb2d990683060c6339d592dcf805ef55c
elarsson <emil_temp92@live.se>
2022-06-30 08:33:06 +0200

    y

--- PasswordGenerator/PasswordGeneratorCommand.cs
+++ PasswordGenerator/PasswordGeneratorCommand.cs
@@ -1,69 +1,31 @@
 using System;
 using System.Management.Automation;
+using PasswordGenerator;
 
 namespace PasswordGenerator
 {
-    [Cmdlet(VerbsCommon.Get, "Password")]
+    [Cmdlet(VerbsCommon.Get,"Password")]
     public class GetGeneratedPasswordCommand : PSCmdlet
     {
         [Parameter(Mandatory = true)]
-        [ValidateRange(6, 30)]
+        [ValidateRange(6,30)]
         public Int32 PwLength { get; set; }
 
         [Parameter(Mandatory = true)]
-
-        public Int32 Amount { get; set; }
-
-        [Parameter(Position = 1)]
-        public SwitchParameter IncludeSpecial
-        {
-            get { return IncludeSpecial; }
-            set { IncludeSpecial = value; }
-        }
-
-        [Parameter(Position = 2)]
-        public SwitchParameter IncludeNumeric
-        {
-            get { return IncludeNumeric; }
-            set { IncludeNumeric = value; }
-        }
-
+        
+        public Int32 Amount { get; set;}
 
         protected override void ProcessRecord()
         {
             for (int i = 0; i < Amount; i++)
             {
-                if (IncludeSpecial & IncludeNumeric)
-                {
-                    var pwd = new Password(PwLength).IncludeLowercase().IncludeUppercase().IncludeSpecial().IncludeNumeric();
-                    var password = pwd.Next();
-                    WriteObject(password);
-                }
-
-                if (IncludeSpecial & !IncludeNumeric)
-                {
-                    var pwd = new Password(PwLength).IncludeLowercase().IncludeUppercase().IncludeSpecial();
-                    var password = pwd.Next();
-                    WriteObject(password);
-                }
-
-                if (IncludeNumeric & !IncludeSpecial)
-                {
-                    var pwd = new Password(PwLength).IncludeLowercase().IncludeUppercase().IncludeNumeric();
-                    var password = pwd.Next();
-                    WriteObject(password);
-                }
-
-                if (!IncludeNumeric & !IncludeSpecial)
-                {
-                    var pwd = new Password(PwLength).IncludeLowercase().IncludeUppercase();
-                    var password = pwd.Next();
-                    WriteObject(password);
-                }
-
+                var pwd = new Password(PwLength).IncludeLowercase().IncludeUppercase().IncludeSpecial().IncludeNumeric();
+                var password = pwd.Next();
+                WriteObject(password);   
             }
-
+            
         }
 
     }
 }
+
