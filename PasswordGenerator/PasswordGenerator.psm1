b5a7a2815d756dffc4ed0c6e9eed727e3b52fac1
elarsson <emil_temp92@live.se>
2022-07-02 16:34:38 +0200

    some updates

---
+++ PasswordGenerator/PasswordGenerator.psm1
@@ -0,0 +1,8 @@
+# Get functions
+Import-Module "$PSScriptRoot\PasswordGenerator\PasswordGenerator.dll" -Force
+
+# Aliases
+New-Alias -Name 'gpwd' -Value "Get-Password"
+
+# Export everything in the directory
+Export-ModuleMember -Function * -Cmdlet * -Alias *
